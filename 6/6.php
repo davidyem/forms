9c30a1fdabc8f27bfc3df98307329ed06ed4ef71
david.yem <davidyeemelyanov@gmail.com>
2016-09-27 18:03:29 +0300

    new commit

--- 6/6.php
+++ 6/6.php
@@ -8,7 +8,7 @@ if (isset($_FILES))
     foreach ($_FILES['file']['name'] as $k=>$v)
     {
 
-        $uploaddir = "../forms/";
+        $uploaddir = "../6/";
         $apend=date('YmdHis').rand(100,1000).'.png';
         $uploadfile = "$uploaddir$apend";
 
--- 6/6_1.php
+++ 6/6_1.php
@@ -1,9 +1,9 @@
 <?php
-$path = $_SERVER['DOCUMENT_ROOT'] . '';
+$path = $_SERVER['DOCUMENT_ROOT'] . '/6/';
 $images = scandir($path);
 if (false !== $images) {
     $images = preg_grep('/\\.(?:png|gif|jpe?g)$/', $images);
     foreach ($images as $image)
-        echo '<img src="//', htmlspecialchars(urlencode($image)),'" width="200" height="200" />';
+        echo '<img src="/6/', htmlspecialchars(urlencode($image)),'" width="200" height="200" />';
 }
 ?>
